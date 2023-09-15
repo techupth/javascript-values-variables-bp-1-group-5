b151fa28d0eb7c8a2058c7075380a72bdc02ff5b
“Phurikanya <phurikanya.ch@gmai.com>
2023-09-15 22:20:32 +0700

    Done All Exercise

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,8 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
-
+let findFahrenheit = (celsius * 1.8) + 32;
 // Start coding here;
-
-let fahrenheit;
+30; //
+(celsius * 1.8) + 32;
+console.log(findFahrenheit)
+let fahrenheit = 86;
